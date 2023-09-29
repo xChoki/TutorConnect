7128eb2ff608e6422d02e272ee721f9a8bc11f16
xChoki <javier.quinmal@hotmail.cl>
2023-09-29 13:16:11 -0300

    fix bug portal reload

--- client/src/components/RequireAuth.jsx
+++ client/src/components/RequireAuth.jsx
@@ -3,22 +3,23 @@ import useAuth from "../hooks/useAuth"
 import { validateRoles } from "../scripts/ValidateRoles"
 
 export default function RequireAuth({ allowedRoles }) {
-  const { auth } = useAuth()
+  const { auth, ready } = useAuth()
   const location = useLocation()
 
   const ValidateResult = validateRoles({ allowedRoles })
-  
+
   // console.log("resultado: " + ValidateResult)
   // console.log("Datos usuario: " + auth.userName + " " + auth.userEmail)
   // console.log("Roles permitidos: " + allowedRoles)
   // console.log("Tiene roles: " + hasRolesObject)
   // console.log("Tiene los roles requeridos: " + hasRequiredRole)
-
-  return ValidateResult ? (
-    <Outlet />
-  ) : auth?.user ? (
-    <Navigate to="/" />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  )
+  if (ready) {
+    return ValidateResult ? (
+      <Outlet />
+    ) : auth?.user ? (
+      <Navigate to="/" />
+    ) : (
+      <Navigate to="/login" state={{ from: location }} replace />
+    )
+  }
 }
