e7010f914287d6d6f7eb8553b15cff358686c549
Marcelo <81325245+half-frozen@users.noreply.github.com>
2023-10-29 19:38:19 -0300

    middleware

---
+++ api/middleware/authHandler.js
@@ -0,0 +1,25 @@
+/* Dependency used to read .env files, in NODE v20.6.0 it is integrated, but we are using v18.17.1LTS and it is not */
+require("dotenv").config()
+
+/* JSONWebToken
+ * Self-contained way for securely transmitting information between parties as a JSON object*/
+const jwt = require("jsonwebtoken") // import
+const jwtSecret = process.env.JWT_SECRET;
+
+
+async function verifyToken(token) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, // token: string,
+        jwtSecret, // secretOrPublicKey: Secret | GetPublicKeyOrSecret,
+        {}, // options?: VerifyOptions & { complete?: false },
+        (err, userData) => {
+        if (err) {
+          reject(err); // If there's an error we send it
+        } else {
+          resolve(userData);
+        }
+      });
+    });
+  }
+
+module.exports = { verifyToken };
