f8bb8450a5f065207cb0024a55ca0f3f74048dc9
Marcelo <81325245+half-frozen@users.noreply.github.com>
2023-10-31 14:52:47 -0300

    verify optimizado

--- api/middleware/authHandler.js
+++ api/middleware/authHandler.js
@@ -5,21 +5,31 @@ require("dotenv").config()
  * Self-contained way for securely transmitting information between parties as a JSON object*/
 const jwt = require("jsonwebtoken") // import
 const jwtSecret = process.env.JWT_SECRET;
+// jwt.verify return a promise
+const verifyTokenAsync = util.promisify(jwt.verify);
 
+// async function verifyToken(token) {
+//     return new Promise((resolve, reject) => {
+//       jwt.verify(token, // token: string,
+//         jwtSecret, // secretOrPublicKey: Secret | GetPublicKeyOrSecret,
+//         {}, // options?: VerifyOptions & { complete?: false },
+//         (err, userData) => {
+//         if (err) {
+//           reject(err); // If there's an error we send it
+//         } else {
+//           resolve(userData);
+//         }
+//       });
+//     });
+//   }
 
 async function verifyToken(token) {
-    return new Promise((resolve, reject) => {
-      jwt.verify(token, // token: string,
-        jwtSecret, // secretOrPublicKey: Secret | GetPublicKeyOrSecret,
-        {}, // options?: VerifyOptions & { complete?: false },
-        (err, userData) => {
-        if (err) {
-          reject(err); // If there's an error we send it
-        } else {
-          resolve(userData);
-        }
-      });
-    });
+  try {
+    const userData = await verifyTokenAsync(token, jwtSecret);
+    return userData;
+  } catch (err) {
+    reject(err); // If there's an error we send it
   }
+}
 
 module.exports = { verifyToken };
